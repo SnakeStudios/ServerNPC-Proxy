a21e1bf84c26b6f6cecb86c12565ac920f560403
Martín R. O <contacto@isnakebuzz.com>
2024-01-28 19:27:48 -0300

    Add: Registering listeners

--- snpcp-velocity/src/main/java/rip/snake/snpcp/velocity/ServerNPC.java
+++ snpcp-velocity/src/main/java/rip/snake/snpcp/velocity/ServerNPC.java
@@ -1,10 +1,12 @@
 package rip.snake.snpcp.velocity;
 
-import com.google.inject.Inject;
-import com.velocitypowered.api.event.proxy.ProxyInitializeEvent;
 import com.velocitypowered.api.event.Subscribe;
+import com.velocitypowered.api.event.proxy.ProxyInitializeEvent;
 import com.velocitypowered.api.plugin.Plugin;
+import com.velocitypowered.api.proxy.ProxyServer;
+import lombok.Getter;
 import org.slf4j.Logger;
+import rip.snake.snpcp.velocity.listener.MessagesListener;
 
 @Plugin(
         id = "snpcp-velocity",
@@ -13,12 +15,20 @@ import org.slf4j.Logger;
         authors = {"iSnakeBuzz_"},
         url = "https://r.snake.rip/snpc"
 )
+@Getter
 public class ServerNPC {
 
-    @Inject
-    private Logger logger;
+    private final ProxyServer proxyServer;
+    private final Logger logger;
+
+    public ServerNPC(ProxyServer server, Logger logger) {
+        this.proxyServer = server;
+        this.logger = logger;
+    }
 
     @Subscribe
     public void onProxyInitialization(ProxyInitializeEvent event) {
+        proxyServer.getEventManager().register(this, new MessagesListener(this));
     }
+
 }
