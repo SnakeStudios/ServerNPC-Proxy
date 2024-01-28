a74efbb50bd05daa111b71df8a3b13b870b7128c
Martín R. O <contacto@isnakebuzz.com>
2024-01-28 19:27:09 -0300

    Add: Message Helper

---
+++ snpcp-commons/src/main/java/rip/snake/servernpc/proxy/core/MessageHelper.java
@@ -0,0 +1,38 @@
+package rip.snake.servernpc.proxy.core;
+
+import com.google.common.io.ByteArrayDataInput;
+import com.google.common.io.ByteStreams;
+import lombok.experimental.UtilityClass;
+import rip.snake.servernpc.proxy.core.enums.SubChannel;
+
+@UtilityClass
+public class MessageHelper {
+
+    public String replace(String message, String playerName) {
+        return message.replace("%player_name%", playerName).replace("%player%", playerName);
+    }
+
+    public NPCRequest decrypt(byte[] data) {
+        ByteArrayDataInput in = ByteStreams.newDataInput(data);
+        String subChannel = in.readUTF();
+
+        boolean isConsole = isBool(in.readUTF());
+        String playerName = in.readUTF();
+        String message = in.readUTF();
+
+        return new NPCRequest(SubChannel.getByName(subChannel), isConsole, playerName, message);
+    }
+
+    private boolean isBool(String s) {
+        boolean bool;
+
+        try {
+            bool = Boolean.parseBoolean(s);
+        } catch (Exception ignored) {
+            bool = false;
+        }
+
+        return bool;
+    }
+
+}
